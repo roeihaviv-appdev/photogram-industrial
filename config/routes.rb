08d49c92d5bb67e5930aeb9d7a97a301f158014c
roeihaviv <rhaviv@chicagobooth.edu>
2023-01-14 15:35:00 +0000

    Generated comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :comments
+  root "photos#index"
   resources :photos
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
