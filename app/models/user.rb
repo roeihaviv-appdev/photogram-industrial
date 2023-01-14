aa06e658ee746d1dc1ff26689a8dd4a628fc61a7
roeihaviv <rhaviv@chicagobooth.edu>
2023-01-14 00:01:25 +0000

    generated models and associations
    Generated photoes updated
    
    Generated comments
    
    Comments logic added
    
    Follow request migrated
    
    Added follow request
    
    Added likes
    
    Added index to comments
    
    Added auto annotate
    
    Annotated models and routes
    
    Added counter caches
    
    Most updated workspace

---
+++ app/models/user.rb
@@ -0,0 +1,38 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                     :bigint           not null, primary key
+#  comments_count         :integer          default(0)
+#  email                  :citext           default(""), not null
+#  encrypted_password     :string           default(""), not null
+#  likes_count            :integer          default(0)
+#  photos_count           :integer          default(0)
+#  private                :boolean
+#  remember_created_at    :datetime
+#  reset_password_sent_at :datetime
+#  reset_password_token   :string
+#  username               :citext
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email                 (email) UNIQUE
+#  index_users_on_reset_password_token  (reset_password_token) UNIQUE
+#  index_users_on_username              (username) UNIQUE
+#
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  has_many :own_photos, class_name: "Photo", foreign_key: "owner_id"
+
+  has_many :comments, foreign_key: :author_id
+
+  has_many :sent_follow_requests, foreign_key: :sender_id, class_name: "FollowRequest"
+
+  has_many :received_follow_requests, foreign_key: :recipient_id, class_name: "FollowRequest"
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
+# == Route Map
+#
+
 Rails.application.routes.draw do
+  resources :likes
+  resources :follow_requests
+  resources :comments
+  root "photos#index"
+  resources :photos
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
