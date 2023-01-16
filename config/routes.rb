ff2caef4690ea08cfaa930839372037ea3e1afa1
roeihaviv <rhaviv@chicagobooth.edu>
2023-01-16 16:32:36 +0000

    Feed works

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,9 @@ Rails.application.routes.draw do
   resources :photos
 
   get ":username/liked" => "photos#liked", as: :liked_photos
+  get ":username/feed" => "users#feed", as: :users_feed
+  get ":username/followers" => "photos#followers", as: :followers_photos
+  get ":username/following" => "photos#following", as: :following_photos
 
   get ":username" => "users#show", as: :user
 end
